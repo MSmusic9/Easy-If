8cbde8690b7a2aad0a43fa129128b189ecfbee19
MSmusic9 <121720254+MSmusic9@users.noreply.github.com>
2023-12-07 19:46:22 +0300

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,16 @@
-from typing import Iterable
+class Iterable:
+  def __instancecheck__(self, value, instance):
+    return "__next__" in dir(value) and "__iter__" in dir(value) and instance == Iterable
 
 
-def cmp(
+class Callable:
+  def __instancecheck__(self, value, instance):
+    return "__call__" in dir(value) and instance == Iterable
+
+
+def draise(err): raise err
+
+
+def final(cls): cls.__init__subclass__ = lambda:draise("cannot implement final class")
+
+
